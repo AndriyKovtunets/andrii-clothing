e5ccf4799f57a0632bc5f017d4ab176ec396fe0c
Andrii <andriy.kovtunets@gmail.com>
2023-07-01 23:12:58 +0200

    created sign up component, the form input component, button component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -17,9 +17,6 @@ const Navigation = () => {
 					<Link className='nav-link' to='/shop'>
 						SHOP
 					</Link>
-				</div>
-
-				<div className='nav-links-container'>
 					<Link className='nav-link' to='/sign-in'>
 						SIGN IN
 					</Link>
