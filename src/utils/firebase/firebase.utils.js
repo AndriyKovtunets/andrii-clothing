8722c92d7a70500c3c86cc23a6784f81e21f53eb
Andrii <andriy.kovtunets@gmail.com>
2023-07-09 21:28:26 +0200

    creates authentication page ccomponent alongside sign form

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,7 @@ import {
 	signInWithPopup,
 	GoogleAuthProvider,
 	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
 } from 'firebase/auth';
 
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
@@ -39,10 +40,11 @@ export const createUserDocumentFromAuth = async (
 	userAuth,
 	additionalInformation = {}
 ) => {
+	if (!userAuth) return;
+
 	const userDocRef = doc(db, 'users', userAuth.uid);
-	console.log(userDocRef);
+
 	const userSnapshot = await getDoc(userDocRef);
-	console.log(userSnapshot.exists());
 
 	//if user data does not exist
 	//create /set the document with the data from userAuth in my collection
@@ -58,7 +60,7 @@ export const createUserDocumentFromAuth = async (
 				...additionalInformation,
 			});
 		} catch (error) {
-			console.error(error);
+			console.log('error creating the user', error.message);
 		}
 	}
 
@@ -68,5 +70,12 @@ export const createUserDocumentFromAuth = async (
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
 	if (!email || !password) return;
+
 	return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+	if (!email || !password) return;
+
+	return await signInWithEmailAndPassword(auth, email, password);
 };
