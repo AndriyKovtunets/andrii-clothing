fc8b1850d5f195ee5a4e12d5508b7c63e49d75de
Andrii <andriy.kovtunets@gmail.com>
2023-08-30 21:25:13 +0200

    adding redux, react-redux, redux-logger and setting up user related redux code

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,9 @@
 import { compose, createStore, applyMiddleware } from 'redux';
+
 import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
 //root-reducer
-
 const middleWares = [logger];
 const composeEnhancers = compose(applyMiddleware(...middleWares));
 
